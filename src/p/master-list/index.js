2207f785cc005c52b3e1967d332cacfcc50ab671
sandzhr0123 <sandzhr0123@gmail.com>
2017-12-16 13:57:07 +0800

    update

--- src/p/master-list/index.js
+++ src/p/master-list/index.js
@@ -10,8 +10,8 @@ var Sticky = require('../../common/sticky');
 var CreateMiniChart = require('./chart/mini-line');
 var tmpl = require('./index.ejs.html');
 
-function TradeList() {
-	TradeList.superclass.constructor.apply(this, arguments);
+function MasterList() {
+	MasterList.superclass.constructor.apply(this, arguments);
 
   if ( Cookie.get('token') ) {
     this._init();
@@ -20,7 +20,7 @@ function TradeList() {
   }
 }
 
-Base.extend(TradeList, Base, {
+Base.extend(MasterList, Base, {
 	_init: function() {
     this._bind();
     this._initSticky();
@@ -29,77 +29,26 @@ Base.extend(TradeList, Base, {
 
   _bind: function() {
       var doc = $(document);
-      this.el = $('#J_OptionBanner');
-      this.category = $(".J_CategoryFilter");
-
-      $('.J_Filter').on('click', $.proxy(this._fold, this));
-      // $('.J_OptionBannerMask').on('click', $.proxy(this._fold, this));
-      $('.J_OptionBannerMask').on('click', $.proxy(this._toggleFilter, this))
-      .on('touchmove', (e) => {
-        e.preventDefault();
-      });
-
-      $('.J_Trigger').on('click', $.proxy(this._toggleFilter, this));
-      $('.J_Item').on('click', $.proxy(this._switch, this));
+      doc.on('tap', '.J_Fn', $.proxy(this._showFilter, this));
+      doc.on('tap', '.J_Item', $.proxy(this._switch, this));
   },
 
-  _fold: function(e) {
-    var bodyEl = $('#J_Page'),
-      htmlEl = $('html'),
-      footerEl = $('footer');
+  _showFilter: function() {
+    var popupEl = $('#J_FnList');
 
-    if (bodyEl.hasClass('move-x')) {
-      $('body').removeClass('unfold');
-
-      footerEl.hide();
-
-      setTimeout(() => {
-        this.el.hide();
-       
-      }, 300);
-
-      this.el.css({
-        top: 0
-      });
-      $('.J_OptionBannerMask').css({
-        top: 0
-      })
-      bodyEl.removeClass('move-x');
-
-      $('.J_OptionBannerMask').hide();
-      bodyEl.css({
-        height: 'auto'
-      });
-
-     setTimeout(()=>{
-       footerEl.show();
-     },500);
-
-    } else {
-      this.el.show();
-      $('body').addClass('unfold');
-      var scrollTop = $(window).scrollTop();
-      this.el.css({
-        top: scrollTop
-      });
-      $('.J_OptionBannerMask').css({
-          top: scrollTop
-      })
-      bodyEl.addClass('move-x');
-
-      bodyEl.css({
-        height: $(window).height(),
-      });
-      $('.J_OptionBannerMask').show();
-
-      footerEl.hide();
-    }
-
+    popupEl.show();
+    setTimeout(() => {
+      popupEl.addClass('show');
+    }, 0)
   },
 
-  _toggleFilter: function(e) {
-      this.el.toggleClass('active');
-      $('.J_OptionBannerMask').toggleClass('active');
+  _hideFilter: function() {
+    var popupEl = $('#J_FnList');
+
+    popupEl.removeClass('show');
+    setTimeout(() => {
+      popupEl.hide();
+    }, 50);
   },
 
   _switch: function(e) {
@@ -108,23 +57,20 @@ Base.extend(TradeList, Base, {
       var curEl = $(e.currentTarget),
           index = curEl.index();
       this.curIndex = index;
+
       if (!curEl.hasClass('active')) {
           curEl.siblings().removeClass('active');
-          curEl.addClass('active');
-
-          $('.cureent', this.el).text(curEl.text());    
+          curEl.addClass('active');  
       }
 
-      // this._fold();
-      this._toggleFilter();
       this._getData();
-
+      this._hideFilter();
   },
 
 	_getData: function() {
 		var params = this._getParams();
-  		var source = this._getSource();
-  		this._request(params, source);
+  	var source = this._getSource();
+  	this._request(params, source);
 	},
 
 	_request: function(params, source) {
@@ -141,60 +87,60 @@ Base.extend(TradeList, Base, {
   			data = data.data;
   			this._render(data);
   		})
-  	},
+  },
 
-  	_render: function(data) {
-	    data = data.map((item) => {
-	        item.img = item.img ? Config.getAvatarPrefix(item.img) : getDefaultAvatarUrl();
-	        return item;
-	    })
+	_render: function(data) {
+    data = data.map((item) => {
+        item.img = item.img ? Config.getAvatarPrefix(item.img) : getDefaultAvatarUrl();
+        return item;
+    })
 
-  		this.render(tmpl, data, $('.J_List'));
-  		this._renderCharts(data);
-  	},
+		this.render(tmpl, data, $('.J_List'));
+		this._renderCharts(data);
+	},
 
-  	_renderCharts: function(data) {
-  		var chartWraperEls = $('.J_TradelistChart', $('.J_List'));
+	_renderCharts: function(data) {
+		var chartWraperEls = $('.J_TradelistChart', $('.J_List'));
 
-  		for ( var i = 0, len = data.length; i < len; i++ ) {
-  			var list = [], profitList = data[i].profit_history || [];
-  			var chartWraperEl = chartWraperEls[i];
-       
-  			for ( var j = 0, le = profitList.length; j < le; j++ ) {
-            if (!isNaN(parseFloat(profitList[j].amount))) {
-              list.push(parseFloat(profitList[j].amount))
-            }
-  			}
-  			new CreateMiniChart({
-  				el: chartWraperEl,
-				  chartName: 'trade-list-charts-rate',
-				  data: list
-  			})
-  		}
-  	},
+		for ( var i = 0, len = data.length; i < len; i++ ) {
+			var list = [], profitList = data[i].profit_history || [];
+			var chartWraperEl = chartWraperEls[i];
+     
+			for ( var j = 0, le = profitList.length; j < le; j++ ) {
+          if (!isNaN(parseFloat(profitList[j].amount))) {
+            list.push(parseFloat(profitList[j].amount))
+          }
+			}
+			new CreateMiniChart({
+				el: chartWraperEl,
+			  chartName: 'trade-list-charts-rate',
+			  data: list
+			})
+		}
+	},
 
-  	_getParams: function() {
-  		var params = new Object();
-  		params.access_token = Cookie.get('token');
-  		return params;
-  	},
+	_getParams: function() {
+		var params = new Object();
+		params.access_token = Cookie.get('token');
+		return params;
+	},
 
-  	_getSource: function() {
-      var activeEl = $('.active', this.category);
-      var index = activeEl.index();
-      var curSource = this.sourceUrls[index];
-  		return curSource;
-  	},
+	_getSource: function() {
+    var activeEl = $('.active', this.category);
+    var index = activeEl.index();
+    var curSource = this.sourceUrls[index];
+		return curSource;
+	},
 
-    _initSticky: function() {
-      $('nav').sticky();
-    },
+  _initSticky: function() {
+    $('nav').sticky();
+  },
 
-  	defaults: function() {
-  		return {
-  			sourceUrls: ['yield_rate','yield_rate_7days', 'yield_rate_30days', 'yield_rate_30days_loss']
-  		}
-  	}	
+	defaults: function() {
+		return {
+			sourceUrls: ['yield_rate','yield_rate_7days', 'yield_rate_30days', 'yield_rate_30days_loss']
+		}
+	}	
 })
 
-new TradeList();
+new MasterList();
