336d0399753539e2ac8e831b17c2bcbc16232b15
sandzhr0123 <sandzhr0123@gmail.com>
2018-01-02 14:47:55 +0800

    'update'

--- src/common/set-range/index.js
+++ src/common/set-range/index.js
@@ -31,9 +31,9 @@ class SetRange extends Base {
 		if (!this.default.isSignBefore) {
 			text = value + ' ' + this.default.sign;
 		}
-
+	
 		this.rangeEl.text(text);
-		// this._setBgColor(value)
+		this._setBgColor(value);
 		this.fire('setRangeSuccess', value);
 	}
 
@@ -41,24 +41,23 @@ class SetRange extends Base {
 		var $el = $('.J_SetController', this.el),
 			$step;
 
-		$step = Math.abs(value / this.default.max * 100 - 3);
+		$step = Math.abs(value / this.default.max);
+		if (parseFloat(this.default.max) <= parseFloat(this.default.placeholder)) {
+			$step = $el.attr('step') / (this.default.max) * 100;
+		}
 
 		if ( this.default.isOpp ) {
-			var max = this.default.max - this.default.min;
-			var _step = max - Math.abs(value);
-			$step = Math.abs( _step / (max) * 100);
+			var _val = value * 100;
+			$step = Math.abs((100 - _val) / this.default.max);
 		}
 
 		if (this.default.isNoMonery) {
 			return;
 		}
-		$el.css({	
-			background: '-o-linear-gradient(left, #BEC3C7 '+ $step +'%, #F2F3F7 '+ $step +'%)',
-			background: '-moz-linear-gradient(left, #BEC3C7 '+ $step +'%, #F2F3F7 '+ $step +'%)',
-			background: '-webkit-gradient(linear, left top, right top, from(#BEC3C7), color-stop('+ $step +', #BEC3C7), color-stop('+ $step +', #F2F3F7), to(#F2F3F7))',
-			background: '-webkit-linear-gradient(to right, #BEC3C7 '+ $step +'%, #F2F3F7 '+ $step +'%)',
-			background: 'linear-gradient(to right, #BEC3C7 '+ $step +'%, #F2F3F7 '+ $step +'%)'
-		});
+
+		$el.css({
+			'backgroundSize': $step + '% 100%'
+		})
 	}
 
 	_requires() {
@@ -68,7 +67,7 @@ class SetRange extends Base {
 
 	_initAttrs() {
 		this.rangeEl = $('.J_RangeValue', this.el);
-		// this._setBgColor(this.data.placeholder);
+		this._setBgColor(this.data.placeholder);
 	}
 
 	defaults() {
@@ -80,7 +79,7 @@ class SetRange extends Base {
 				placeholder: 100,
 				minMonery: 1000, // 离最小下单相差金额
 				isNoMonery: false, //是否资金充足
-				isOpp: false, // 是否类似(-100% - 0)
+				isOpp: false, // 是否类似(-100px - 0)
 				isSignBefore: true, // 符号是在前还是后 默认前
 				sign: '$'
 			}
