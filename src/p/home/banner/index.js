7684897e542504ecd81a3011fa060375865f12bd
sandzhr0123 <sandzhr0123@gmail.com>
2018-01-10 13:18:39 +0800

    'home-

--- src/p/home/banner/index.js
+++ src/p/home/banner/index.js
@@ -6,13 +6,28 @@ import '../../../common/slider';
 
 export default class HomeBanner extends Base {
 	constructor(config) {
-		super(config);
-		this.render(tmpl, {}, $('.km-slider-outer'));
-		$('#slider').slider({
-            loop: true,
-            play: true,
-            interval: 15 * 1000,
-            duration: 1000
-        });
-	}
+        super(config);
+        this._getData();
+    }
+
+    _getData() {
+        var self = this;
+        this.ajax({
+            url: '/v1/user/banner/home/',
+            data: {
+                wl: getWXWL()
+            }
+        }).then(function(data) {
+            data = data.data;
+            self.render(tmpl, data, $('.km-slider-outer'));
+            if (data.length > 0) {
+                $('#slider').slider({
+                    loop: true,
+                    play: true,
+                    interval: 15 * 1000,
+                    duration: 1000
+                });
+            }  
+        })
+    }
 }
