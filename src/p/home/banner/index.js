747b1203e315e89f092596a8c8d360dbf643de7a
sandzhr0123 <sandzhr0123@gmail.com>
2018-01-12 11:53:56 +0800

    'update'

--- src/p/home/banner/index.js
+++ src/p/home/banner/index.js
@@ -32,7 +32,7 @@ export default class HomeBanner extends Base {
                 $('#slider').slider({
                     loop: true,
                     play: true,
-                    interval: 15 * 1000,
+                    interval: 10 * 1000,
                     duration: 1000
                 });
             }  
