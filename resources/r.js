4dadd9e3dc6c123881e9131a66d316e8250f6d51
sandzhr0123 <sandzhr0123@gmail.com>
2018-03-19 11:24:24 +0800

    'update'

--- resources/r.js
+++ resources/r.js
@@ -9,12 +9,12 @@ function getBottomNavPages() {
 
 // 默认支付方式
 function getDefaultPayWay() {
-	return 'weixin'
+	return 'pc'
 }
 
 // 显示哪几种支付方式, 与getPayUrl()中对应
 function getShowPayWay() {
-	return ['weixin', 'pc'] //, 'zhifubao',  'weixinWeb', 'zhifubaoWeb', 'kuaijie',
+	return ['pc'] //, 'zhifubao', 'weixin',   'weixinWeb', 'zhifubaoWeb', 'kuaijie',
 }
 
 /**
